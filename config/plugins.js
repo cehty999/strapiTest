ac21c2ad1972d8aa280c9e4396f1888c59898c11
Cehty <cehty11@gmail.com>
2024-02-01 16:12:54 +0300

    add collection type

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = () => ({
+    // ...
+
+    "firebase-auth": {
+        enabled: true,
+        config:{ FIREBASE_JSON_ENCRYPTION_KEY:"encryptMe" }
+    },
+
+    // ...
+});
--- src/api/one-news/controllers/one-news.js
+++ src/api/one-news/controllers/one-news.js
@@ -1,9 +1,22 @@
 'use strict';
-
 /**
- * one-news controller
+ *  one-news controller
  */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::one-news.one-news');
+module.exports = createCoreController('api::one-news.one-news', ({ strapi }) => ({
+    async create(ctx) {
+        let users = await strapi.entityService.findMany('plugin::users-permissions.user');
+
+        users.forEach(user => {
+            strapi.notification.sendNotification(user.fcm, {
+                notification: {
+                    title: "Liked your quote",
+                    body: "Liked your quote"
+                }
+            });
+        });
+
+        // ctx.body = result;
+    }
+}));
